40bd70d394c89c4fca3bd6621d33222ba8d779e2
Cerne17 <miguelbadanycerne.20231@poli.ufrj.br>
2023-07-06 09:29:25 -0300

    Concluí o design da página (exceto pelo blur do fundo, que eu vou tentar incluir caso haja tempo). Comecei a desenvolver a lógica em JS: implementei uma classe para pegar as informações do html, preciso implementar o evento e algumas checagens.

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 "use strict";
 
-function getValues() {
-    const user     = getElementById("user");
-    const password = getElementById("password");
+// function getValues() {
+//     const user     = getElementById("user");
+//     const password = getElementById("password");
+//     const checkbox = getElementById("remember");
+//     return
+// }
+
+class Informations {
+    constructor() {
+        this.user = getElementById("user");
+        this.password = getElementById("password");
+        this.checkbox = getElementById("remember");
+    }
+    checarTipoUsuario () {
+        if (this.user.includes('@')) {
+            return "E-mail: ";
+        } else {
+            return "Nome de Usuário: "
+        }
+    }
+
+    alerta () {
+        let mensagem = `${this.checarTipoUsuario()}${this.user}\nSenha: ${this.password}\n${this.checkbox}`
+    }
+
 }
+
