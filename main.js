5d119e4476959664c3516e35b2589b64ab9354b7
Cerne17 <miguelbadanycerne.20231@poli.ufrj.br>
2023-07-06 10:20:02 -0300

    Checagens na classe foram concluídas, tenho que resolver o problema das mensagens não aparecerem de maneira correta na tela. Terminar a parte do alert

--- main.js
+++ main.js
@@ -9,21 +9,73 @@
 
 class Informations {
     constructor() {
-        this.user = getElementById("user");
-        this.password = getElementById("password");
-        this.checkbox = getElementById("remember");
+        this.user     = document.getElementById("user");
+        this.password = document.getElementById("password");
+        this.checkbox = document.getElementById("remember");
+        this.sucesso  = document.getElementById("sucesso");
+        this.falha    = document.getElementById("falha");
     }
     checarTipoUsuario () {
+
+        // Checa se o usuário inseriu um email ou um usuário, para formatar a mensagem de alerta
+
         if (this.user.includes('@')) {
             return "E-mail: ";
         } else {
             return "Nome de Usuário: "
         }
     }
+    validacaoForm () {
 
-    alerta () {
-        let mensagem = `${this.checarTipoUsuario()}${this.user}\nSenha: ${this.password}\n${this.checkbox}`
+        // Checa se todos campos com 'required' foram devidamente preenchidos
+
+        let inputsNecessarios = document.querySelectorAll("#formulario input[required]");
+        let validacao = true;
+
+        for (let i = 0; i < inputsNecessarios.length; i++) {
+            if (!inputsNecessarios[i].validity.valid) {
+                validacao = false;
+                break;
+            }
+        }
+        return validacao;
     }
 
+    alerta () {
+
+        if (this.validacaoForm()) {
+            let mensagem = `${this.checarTipoUsuario()}${this.user}\nSenha: ${this.password}\nLembrar de mim: ${this.checkbox}`;
+            alert(mensagem);
+
+            // Exibição da mensagem de sucesso e omissão da de erro:
+
+            if (Array.from(this.sucesso.classList).includes("hidden")) {
+                this.sucesso.classList.remove("hidden");
+            }
+            if (!Array.from(this.falha.classList).includes("hidden")) {
+                this.falha.classlist.addClass("hidden");
+            }
+        } else {
+            
+            // Exibição da mensagem de erro e omissão da de sucesso:
+
+            if (!Array.from(this.sucesso.classList).includes("hidden")) {
+                this.sucesso.classList.remove("hidden");
+            }
+            if (Array.from(this.falha.classList).includes("hidden")) {
+                this.falha.classlist.addClass("hidden");
+            }
+        }
+
+        // Atualização dos estados dos botões:
+        this.sucesso = document.getElementById("sucesso");
+        this.falha   = document.getElementById("falha");
+
+    }
 }
 
+document.getElementById("formulario").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita falsas ativações do botão
+    const informacoes = new Informations();
+    informacoes.alerta();
+});
